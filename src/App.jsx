d2c9be60a47f2c2ad2d2c9c7205ae7fe5074b903
ajente0 <aj0speedrun@gmail.com>
2023-05-25 14:14:46 +0200

    4

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {useState} from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import {Router, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Lista from './components/Lista';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,11 +29,11 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
 
-      <Router>
-        <Route path="/lista" exact component={<Lista />} />
-      </Router>
+      <Routes>
+        <Route path="/el2/lista" element={<Lista />} />
+      </Routes>
     </>
   );
 }
